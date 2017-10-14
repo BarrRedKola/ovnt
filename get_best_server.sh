b263d0e6a812effcd23f920ee413023abf1c948d
set <lc1986@gmail.com>
2017-10-14 19:23:41 +0100

    Add new function that checks the RTT for VPN services defined by the nordVPN files in config

---
+++ get_best_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source config
+
+best_server=""
+min=10000
+
+for i in $(ls  $VPN_ROOT|grep -i "${VPN_COUNTRY}"|grep udp)
+do
+  server=${i##*/}
+  server=${server%.udp1194.ovpn}
+  echo -n "Min RTT to $server:  "
+  current_min=$(ping -c3 $server|grep min|cut -d '=' -f 2|cut -d '/' -f 1|awk '{print $1}')
+  echo $current_min
+  if (( $(echo "$min > $current_min" |bc -l) ))
+  then
+    min=$current_min
+    best_server=$i
+  fi
+done
+
+echo "Best server with a min RTT ${min} is:  ${best_server}"
+echo $best_server > last_best_server
+
