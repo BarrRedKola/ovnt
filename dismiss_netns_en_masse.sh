5505734765f1c29619475d8b0c57b85d21a72f1c
set <lc1986@gmail.com>
2017-10-14 19:24:48 +0100

    Make more concrete the kill commands to avoid killing something else

--- dismiss_netns_en_masse.sh
+++ dismiss_netns_en_masse.sh
@@ -3,16 +3,24 @@
 IP=/usr/bin/ip
 NETNS=torrent
 
-echo "Kill transmission"
-kill -9 $(ps |grep transmission|grep -v grep|cut -d ' ' -f 1)
+echo "Kill all possible transmission processes"
+for i in $(ps |grep transmission|grep -v 'grep transmission'|awk '{print $1}')
+do
+  kill -9 $i
+done
+echo "Remove all possible socat port forwarding"
+for i in $(ps |grep "socat tcp-listen:9091,reuseaddr,fork tcp-connect:10.200.1.2:9091"|grep -v 'grep socat'|awk '{print $1}')
+do
+  kill -9 $i
+done
 
-echo "Remove socat port forwarding"
-kill -9 $(ps |grep socat|grep -v grep|cut -d ' ' -f 1)
-
-
-echo "Kill VPN process"
-kill -9 $(ps |grep openvpn|grep -v grep|cut -d ' ' -f 1)
-
+#get the used vpn_service
+vpn_service=$(cat last_best_server)
+echo "Kill all possible VPN processes for the given VPN service ${vpn_service}"
+for i in $(ps |grep openvpn | grep "${vpn_service}"|grep -v 'grep'|awk '{print $1}')
+do
+  kill -9 $i
+done
 
 echo "Delete the whole netns"
 $IP netns delete $NETNS
