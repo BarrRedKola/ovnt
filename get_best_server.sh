67b54db232e0f1400153bf27ed2335af5dd1b286
set <lc1986@gmail.com>
2017-10-15 11:47:44 +0100

    Fix

--- get_best_server.sh
+++ get_best_server.sh
@@ -10,13 +10,20 @@ do
   server=${i##*/}
   server=${server%.udp1194.ovpn}
   echo -n "Min RTT to $server:  "
-  current_min=$(ping -c3 $server|grep min|cut -d '=' -f 2|cut -d '/' -f 1|awk '{print $1}')
-  echo $current_min
-  c=$(echo "$min > $current_min" |bc -l)
-  if [ $c -eq 1 ]
+  tmp_current_min=$(ping -c3 $server 2>/dev/null)
+  success=$(echo $?)
+  current_min=$(echo $tmp_current_min|awk -F "=" '{print $NF}'|cut -d '/' -f 1|awk '{print $1}')
+  if [ $success -eq 0 ]
   then
-    min=$current_min
-    best_server=$i
+    echo $current_min
+    c=$(echo "$min > $current_min" |bc -l)
+    if [ $c -eq 1 ]
+    then
+      min=$current_min
+      best_server=$i
+    fi
+  else
+    echo "CANNOT BE REACHED"
   fi
 done
 
